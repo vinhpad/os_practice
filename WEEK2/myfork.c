ffebb50f81c9fc394c3b9f6bf6a1e5c2bcfce69c
vinhpad <phamanducvinh@gmail.com>
2024-02-24 17:52:46 +0700

    fork

---
+++ WEEK2/myfork.c
@@ -0,0 +1,36 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+int value = 5;
+int main(){
+    printf("\n Before fork()");
+    pid_t pid;
+    pid = fork();
+    printf("\n ProcessID = %d", getpid());
+    
+
+    if (pid < 0) { /* error occurred */
+        fprintf(stderr, "\n Fork Failed");
+        return 1;
+    } else 
+        if (pid == 0) { /* child process */
+          
+            printf("\n Parent of ProcessID %d", getppid());
+            value += 15;
+            printf("\n CHILD: value = %d \n",value);
+            /* LINE A */
+            return 0;
+        } else 
+            if (pid > 0) { /* parent process */
+                wait(NULL);
+            
+                printf("\n Parent of ProcessID %d", getppid());
+                printf("\n PARENT: value = %d ",value);
+                printf("\n PARENT: end process %d \n", getpid());
+                return 0;
+            }
+
+    
+    return 0;
+}
